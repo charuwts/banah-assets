73d1e94dbec6a3e0385349c4fc89d76cfd3f6f80
Brandon White <brandonwhite@charuwts.com>
2020-04-29 22:08:21 -0600

    working import

---
+++ js/index.js
@@ -0,0 +1,31 @@
+// #####
+// Builder
+// #####
+$( document ).on('turbolinks:load', function() {
+  var $builderLayout = $("#builder-layout");
+  var $add  = $("#add-row");
+  
+  function banahFadeIn() {
+    let tl = gsap.timeline();
+    .from(".banah-fade-in", {duration: 0.8, y: "-0.3rem", opacity: 0});
+  }
+
+  function createTile(event) {
+      event ? event.preventDefault() : '';
+      var element = $("<div></div>").addClass("demo-row shadow-sm p-2 my-1").html('demo-row');
+      $builderLayout.append(element)
+  
+      Draggable.create(element, {
+          onRelease: onRelease,
+          bounds: $builderLayout
+      })
+      function onRelease() {
+          gsap.to(this.target, 0.5, {x:0, y:0})
+      }
+  }
+  if ($builderLayout.length > 0) {
+      $add.click(createTile);
+      createTile();
+  }
+  banahFadeIn();
+});
